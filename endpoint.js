d6f01309d137fcd4dd3a1ca26badc0ef90f28c3a
onemoresn <onemoresn@gmail.com>
2026-06-15 02:57:29 -0400

    Update SiteCommand API URL to systemwatch.onrender.com

--- endpoint.js
+++ endpoint.js
@@ -4,5 +4,5 @@
   var isLocal = host === 'localhost' || host === '127.0.0.1';
   window.SiteCommandEndpoint =
     window.SITECOMMAND_API ||
-    (isLocal ? 'http://localhost:3001/v1/events' : 'https://sitecommand-api.onrender.com/v1/events');
+    (isLocal ? 'http://localhost:3001/v1/events' : 'https://systemwatch.onrender.com/v1/events');
 })();
